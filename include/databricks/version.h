4ec21f6dc0760b056e9ecc10f33c5a40b40a3907
Calvin Min <calvin.min@databricks.com>
2025-10-21 11:43:57 -0400

    v0.2.2 Doc Update

--- include/databricks/version.h
+++ include/databricks/version.h
@@ -11,9 +11,9 @@ namespace databricks
     /**
      * @brief SDK version information
      */
-    constexpr const char *VERSION = "0.2.0";
+    constexpr const char *VERSION = "0.2.2";
     constexpr int VERSION_MAJOR = 0;
     constexpr int VERSION_MINOR = 2;
-    constexpr int VERSION_PATCH = 0;
+    constexpr int VERSION_PATCH = 2;
 
 } // namespace databricks
